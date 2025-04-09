e6c2ba7c93f52d698e351956f1c2381e22202f29
DarshanS-cmd <darshanshakthi09@gmail.com>
2025-04-09 13:08:18 +0530

    Update feedback_entry.py

--- feedback_entry.py
+++ feedback_entry.py
@@ -1,1 +1,9 @@
+def collect_feedback():
+    feedback_list = []
+    while True:
+        feedback = input("Enter student feedback (or type 'done' to finish): ")
+        if feedback.lower() == 'done':
+            break
+        feedback_list.append(feedback)
+    return feedback_list
 
